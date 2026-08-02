68587ebf14386f7dcfbb12efe08cacd24d73022d
William Felipe <williamfelipe081@gmail.com>
2026-08-02 19:21:32 -0300

    feat: create addres entitiy

---
+++ src/main/java/com/events/api/domain/address/Address.java
@@ -0,0 +1,28 @@
+package com.events.api.domain.address;
+
+import com.events.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String uf;
+    private String city;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
