a79c4b91717d8f1067a4577dbb6d75be0a3076ed
William Felipe <williamfelipe081@gmail.com>
2026-08-02 19:27:22 -0300

    feat: create coupon repository

---
+++ src/main/java/com/events/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.events.api.repositories;
+
+import com.events.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
