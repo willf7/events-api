6fb198087f818f3386c8fc2e2eade3ff61d0df53
William Felipe <williamfelipe081@gmail.com>
2026-08-02 19:27:39 -0300

    feat: create event repository

---
+++ src/main/java/com/events/api/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.events.api.repositories;
+
+import com.events.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
