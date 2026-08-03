a62ebd6a8cb1ae4b48ff5f27e1febd6ec658b6ff
William Felipe <williamfelipe081@gmail.com>
2026-08-03 20:47:14 -0300

    feat: add address relation to event

--- src/main/java/com/events/api/domain/event/Event.java
+++ src/main/java/com/events/api/domain/event/Event.java
@@ -1,9 +1,7 @@
 package com.events.api.domain.event;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.events.api.domain.address.Address;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,4 +27,7 @@ public class Event {
     private String eventUrl;
     private Boolean remote;
     private Date date;
+
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private Address address;
 }
