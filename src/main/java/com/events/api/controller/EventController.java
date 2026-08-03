bbe2646c255293f16ac6d2a305a8428095750e76
William Felipe <williamfelipe081@gmail.com>
2026-08-03 20:49:50 -0300

    feat: create event controller

---
+++ src/main/java/com/events/api/controller/EventController.java
@@ -0,0 +1,81 @@
+package com.events.api.controller;
+
+import com.events.api.domain.event.Event;
+import com.events.api.domain.event.EventDetailsDto;
+import com.events.api.domain.event.EventRequestDTO;
+import com.events.api.domain.event.EventResponseDTO;
+import com.events.api.service.EventService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Event> create(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("eventUrl") String eventUrl,
+            @RequestParam("city") String city,
+            @RequestParam("state") String state,
+            @RequestParam("remote") Boolean remote,
+            @RequestParam("date") Long date,
+            @RequestParam("image") MultipartFile image
+    ) {
+        EventRequestDTO eventRequestDTO = new EventRequestDTO(title, description, image, eventUrl, remote, city, state, date);
+        Event newEvent = eventService.createEvent(eventRequestDTO);
+        return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        List<EventResponseDTO> events = eventService.getUpcomingEvents(page, size);
+        return ResponseEntity.ok(events);
+    }
+
+    @GetMapping("filter")
+    public ResponseEntity<List<EventResponseDTO>> filterEvents(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String city,
+            @RequestParam(required = false) String uf,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate
+    ) {
+        title = (title != null) ? title : "";
+        city = (city != null) ? city : "";
+        uf = (uf != null) ? uf : "";
+        startDate = (startDate != null) ? startDate : new Date(0);
+        endDate = (endDate != null)
+                ? endDate
+                : Date.from(
+                LocalDate.now()
+                        .plusYears(100)
+                        .atStartOfDay(ZoneId.systemDefault())
+                        .toInstant()
+        );
+
+        List<EventResponseDTO> events = eventService.getFilteredEvents(page, size, title, city, uf, startDate, endDate);
+        return ResponseEntity.ok(events);
+    }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventDetailsDto> getEvent(@PathVariable("eventId") UUID eventId) {
+        EventDetailsDto event = eventService.getEventDetails(eventId);
+        return ResponseEntity.ok(event);
+    }
+}
