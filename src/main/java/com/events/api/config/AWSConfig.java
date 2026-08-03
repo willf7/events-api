b7febf8facb80c40e30303380affe176f5623a31
William Felipe <williamfelipe081@gmail.com>
2026-08-03 20:51:43 -0300

    feat: create AWSConfig and initial instance

---
+++ src/main/java/com/events/api/config/AWSConfig.java
@@ -0,0 +1,23 @@
+package com.events.api.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 createAmazonS3Instance() {
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withRegion(awsRegion)
+                .build();
+    }
+}
