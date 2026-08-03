9e5f32cd88eae53bee94be869f2a632574199636
William Felipe <williamfelipe081@gmail.com>
2026-08-03 20:50:20 -0300

    feat: create coupon service

---
+++ src/main/java/com/events/api/service/CouponService.java
@@ -0,0 +1,40 @@
+package com.events.api.service;
+
+import com.events.api.domain.coupon.Coupon;
+import com.events.api.domain.coupon.CouponRequestDTO;
+import com.events.api.domain.event.Event;
+import com.events.api.repositories.CouponRepository;
+import com.events.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CouponService {
+    @Autowired
+    private CouponRepository repository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO data) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found."));
+
+        Coupon newCoupon = new Coupon();
+
+        newCoupon.setCode(data.code());
+        newCoupon.setDiscount(data.discount());
+        newCoupon.setEvent(event);
+        newCoupon.setValid(new Date(data.valid()));
+
+        return repository.save(newCoupon);
+    }
+
+    public List<Coupon> getCouponsByEvent(UUID eventId, Date currentDate) {
+        return repository.findByIdAndValidAfter(eventId, currentDate);
+    }
+}
