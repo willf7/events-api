5248a3822bec335f75c1b6bdd4c9151a93041f60
William Felipe <williamfelipe081@gmail.com>
2026-08-03 20:46:18 -0300

    feat: add findByIdAndValidAfter method to coupon repository

--- src/main/java/com/events/api/repositories/CouponRepository.java
+++ src/main/java/com/events/api/repositories/CouponRepository.java
@@ -2,8 +2,13 @@ package com.events.api.repositories;
 
 import com.events.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+    @Query("SELECT c FROM Coupon c WHERE c.event.id = :eventId AND c.valid >= :currentDate")
+    List<Coupon> findByIdAndValidAfter(UUID eventId, Date currentDate);
 }
