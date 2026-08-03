74eadc072bf02605036441578149968b6aabd0ae
William Felipe <williamfelipe081@gmail.com>
2026-08-03 20:46:41 -0300

    feat: add findUpcomingEvents and findFilteredEvents methods to event repository

--- src/main/java/com/events/api/repositories/EventRepository.java
+++ src/main/java/com/events/api/repositories/EventRepository.java
@@ -1,9 +1,28 @@
 package com.events.api.repositories;
 
 import com.events.api.domain.event.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
+    @Query("SELECT e FROM Event e LEFT JOIN e.address a WHERE e.date >= :currentDate")
+    Page<Event> findUpcomingEvents(@Param("currentDate") Date currentDate, Pageable pageable);
+
+    @Query("SELECT e FROM Event e LEFT JOIN e.address a " +
+            "WHERE (:title IS NULL OR e.title LIKE %:title%) " +
+            "AND (:city IS NULL OR a.city LIKE %:city%) " +
+            "AND (:uf IS NULL OR a.uf LIKE %:uf%) " +
+            "AND (e.date >= :starDate AND e.date <= :endDate)")
+    Page<Event> findFilteredEvents(@Param("title") String title,
+                                   @Param("city") String city,
+                                   @Param("uf") String uf,
+                                   @Param("startDate") Date startDate,
+                                   @Param("endDate") Date endDate,
+                                   Pageable pageable);
 }
